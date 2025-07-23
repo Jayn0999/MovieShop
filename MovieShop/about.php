74def12959011ca26a360ca5bb93e4ce935598bf
Mary Jane Valeros <148967105+Jayn0999@users.noreply.github.com>
2025-07-23 22:16:57 +0800

    Add files via upload

---
+++ MovieShop/about.php
@@ -0,0 +1,72 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">home</a> / about </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/about-img.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>We are an online movie shop dedicated to provide you with the ultimate movie-watching experience. With a passion for film and a commitment to customer satisfaction, we aim to be your go-to destination for all your movie needs.</p>
+         <p>We understand the importance of convenience in today's fast-paced world, which is why we have designed our platform to be user-friendly and accessible. You can browse our extensive library, search for specific titles, and filter movies based on various criteria. We provide detailed descriptions, ratings, and reviews to help you make informed decisions about what to watch next.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
